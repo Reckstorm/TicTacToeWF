db26eeea9f4a6a7ee35b7604a5d7aaec9ce2b5c4
Mihail Sevagin <lolvillian@gmail.com>
2023-03-30 21:19:39 +0300

    Field created
    Made a field with buttons 3x3

--- TicTacToeWF/Form1.Designer.cs
+++ TicTacToeWF/Form1.Designer.cs
@@ -30,8 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(170, 200);
+            this.Text = "Tic-Tac-Toe";
+
+            this.BackColor = Color.Yellow;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+
+            this.MaximumSize = new Size(170, 200);
+            this.MinimumSize = new Size(170, 200);
+
+            List<Button> buttons = new List<Button>();
+
+            for (int i = 0, x = 0, y = 0; i < 9; i++)
+            {
+                Button temp = new Button();
+                temp.Text = " ";
+                temp.ForeColor = Color.White;
+                temp.Size = new Size(50, 50);
+                temp.Font = new Font("Arial", 15, FontStyle.Bold);
+                temp.BackColor = Color.Black;
+                temp.Location = new Point(x, y); 
+                buttons.Add(temp);
+                if (i == 2 || i == 5) x = 0;
+                else x += 50;
+                if (i == 2 || i == 5 || i == 8) y += 50;
+            }
+
+            foreach (Button button in buttons) this.Controls.Add(button);
         }
 
         #endregion
